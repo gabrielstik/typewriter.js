3e6cfc407f2ff23bacf4a7c1ab9856ea4339d6fe
gabrielstik <gabriel.stik@gmail.com>
2017-06-09 23:27:59 +0200

    :bug: Fixed scope bug
    Signed-off-by: gabrielstik <gabriel.stik@gmail.com>

--- scripts/type.js
+++ scripts/type.js
@@ -2,17 +2,16 @@ function type() {
   let element = document.querySelector(".type") // Your element
   let content = element.innerHTML // Getting the content of your element
   let lenght = content.length // Getting its length
-  let start_interval, type_interval
   element.innerHTML = "" // Setting its content to void
 
   if (element.getAttribute("type-start")) {
     // If attributes are not empty
-    let start_interval = element.getAttribute("type-start") // Interval before begining
-    let type_interval = element.getAttribute("type-interval") // Interval between chars
+    start_interval = element.getAttribute("type-start") // Interval before begining
+    type_interval = element.getAttribute("type-interval") // Interval between chars
   } else {
     // If they are, use default values
-    let start_interval = 1000 // Setting default values
-    let type_interval = 100 // Idem
+    start_interval = 1000 // Setting default values
+    type_interval = 100 // Idem
   }
 
   let i = 0 // Setting up loop increment to 0
